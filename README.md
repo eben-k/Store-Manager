101694751b924581ffc68c027d6f9ad64706779d
eben-k <ennim.consults@gmail.com>
2018-10-18 15:20:10 +0000

    add coverage badge to readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![Coverage Status](https://coveralls.io/repos/github/eben-k/Store-Manager/badge.svg?branch=ch-integrate-coverage)](https://coveralls.io/github/eben-k/Store-Manager?branch=ch-integrate-coverage)
+[![Build Status](https://travis-ci.com/eben-k/Store-Manager.svg?branch=ch-integrate-coverage)](https://travis-ci.com/eben-k/Store-Manager)
+
 # Store-Manager
 Help store owners manage sales and product inventory records. Single store use.
---
+++ server/test/app.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+// const app = require('../app');
+
+describe('Basic Mocha String Test', () => {
+  it('should return -1 when "Hello" is missing', () => {
+    assert.equal(-1, 'Hallo World'.indexOf('Hello'));
+  });
+  it('should return number of characters in a string', () => {
+    assert.equal('Hello'.length, 5);
+  });
+
+  it('should return first character of the string', () => {
+    assert.equal('Hello'.charAt(0), 'H');
+  });
+});
