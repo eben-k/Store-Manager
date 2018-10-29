c495c278046d0de2390940e64bf7d3c8b8e96c61
eben-k <ennim.consults@gmail.com>
2018-10-29 08:30:14 +0000

    [Chore #161525298]setup database

--- server/app.js
+++ server/app.js
@@ -2,23 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import expressValidator from 'express-validator';
-import routes from './routes/index';
+import index from './routes/index';
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
 
-app.use('/', routes);
-app.use('/api/v1/', (req, res) => {
+app.use('/', index);
+app.use('*', (req, res) => {
   res.status(200).json('Welcome to Store Manager!');
 });
 
-app.get('/', (req, res) => res.send('Hello World'));
 
 dotenv.config();
 
